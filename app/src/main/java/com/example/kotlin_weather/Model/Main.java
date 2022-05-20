120f29e594ca5ea275fd20bbc9e0b2251fd51030
Admin <traiquelua99@gmail.com>
2022-05-20 13:32:28 +0700

    add textview, *c, sửa ngày giờ

--- app/src/main/java/com/example/kotlin_weather/Model/Main.java
+++ app/src/main/java/com/example/kotlin_weather/Model/Main.java
@@ -18,37 +18,29 @@ public class Main {
     @Expose
     private Double tempMax;
 
-    public Double getTemp() {
+    public double getTemp() {
         return temp;
     }
 
-    public void setTemp(Double temp) {
-        this.temp = temp;
-    }
+
 
     public Double getFeelsLike() {
         return feelsLike;
     }
 
-    public void setFeelsLike(Double feelsLike) {
-        this.feelsLike = feelsLike;
-    }
+
 
     public Double getTempMin() {
         return tempMin;
     }
 
-    public void setTempMin(Double tempMin) {
-        this.tempMin = tempMin;
-    }
+
 
     public Double getTempMax() {
         return tempMax;
     }
 
-    public void setTempMax(Double tempMax) {
-        this.tempMax = tempMax;
-    }
+
 
 
 }
